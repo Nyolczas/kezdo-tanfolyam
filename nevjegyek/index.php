632ffd4d163e1cdeb5e51b1ccc94699d00535d3a
Nyolczas <nyolczas@gmail.com>
2017-11-07 23:56:32 +0100

    nevjegyek

---
+++ nevjegyek/index.php
@@ -0,0 +1,43 @@
+<?php
+require("kapcsolat.php");
+
+//print_r ($_POST);
+$kifejezes = (isset($_POST['kifejezes'])) ? $_POST['kifejezes'] : "";
+$sql = "SELECT *
+        FROM nevjegyek
+        WHERE (
+            nev LIKE '%{$kifejezes}%'
+            OR cegnev LIKE '%{$kifejezes}%'
+            OR mobil LIKE '%{$kifejezes}%'
+            OR email LIKE '%{$kifejezes}%'
+            )
+        ORDER BY nev ASC";
+$eredmeny = mysqli_query($dbconn, $sql);  
+$kimenet = "";
+while ($sor = mysqli_fetch_assoc($eredmeny)) {
+    $kimenet.= "<article>
+        <h2>{$sor['nev']}</h2>
+        <h3>{$sor['cegnev']}</h3>
+        <p><a href=\"tel:{$sor['mobil']}\">Mobil: {$sor['mobil']}</a></p>
+        <p><a href=\"mailto: {$sor ['email']}\">Email: {$sor ['email']}</a></p>
+        </article>\n";
+        // print_r($sor);
+}
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Névjegykártyák</title>
+    <link rel="stylesheet" href="stilus.css">
+</head>
+<body>
+    <h1>Névjegykártyák</h1>
+    <form method="post" action="">
+        <input type="search" id="kifejezes" name="kifejezes">
+    </form>
+    <?php print $kimenet; ?>
+    
+</body>
+</html>
---
+++ nevjegyek/sql-parancsok.sql
@@ -0,0 +1,40 @@
+-- Minden érték lekérése
+SELECT *
+FROM tablanev
+
+-- Választott értékek lekérése
+SELECT oszlop1, oszlop2, oszlop3
+FROM tablanev
+
+-- Lekérdezés feltétellel
+SELECT * 
+FROM tablanev
+WHERE feltetel
+
+-- Keresés LIKE segítségével
+SELECT oszlop1, oszlop2, oszlop3
+FROM tablanev
+WHERE oszlop1 LIKE kifejezes
+
+SELECT oszlop1, oszlop2, oszlop3
+FROM tablanev
+WHERE oszlop1 LIKE %kifejezes%
+
+-- Rendezés ASC [A-Z], DESC [Z-A]
+SELECT * 
+FROM tablanev
+ORDER BY oszlop1 ASC
+
+SELECT * 
+FROM tablanev
+ORDER BY oszlop1 DESC
+
+SELECT * 
+FROM tablanev
+WHERE feltetel
+ORDER BY oszlop1 DESC
+
+-- Adott darab sor lekérdezése
+SELECT * 
+FROM tablanev
+LIMIT 2
