524ef9c4fac44a2a312db7e0198b47ce8ea41050
Nyolczas <nyolczas@gmail.com>
2017-11-13 20:47:21 +0100

    felvitel

--- nevjegyek/admin/felvitel.php
+++ nevjegyek/admin/felvitel.php
@@ -10,9 +10,17 @@
 <body>
     <h1>Névjegykártyák</h1>
     <form method="post" action="">
-        <p> <label for="">Név:</label>
+        <p> <label for="nev">Név:</label>
             <br>
-            <input type="text">
+            <input type="text" id="nev" name="nev">
+        </p>
+        <p> <label for="cegnev">Cégnév:</label>
+            <br>
+            <input type="text" id="cegnev" name="cegnev">
+        </p>
+        <p> <label for="mobil">Mobil:</label>
+            <br>
+            <input type="tel" id="mobil" name="mobil">
         </p>
     </form>
 </body>
