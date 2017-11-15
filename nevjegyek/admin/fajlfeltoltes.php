36e35c1659645758a3a5bc127b596b9969d169e7
Nyolczas <nyolczas@gmail.com>
2017-11-16 00:01:25 +0100

    Fájlfeltöltés

---
+++ nevjegyek/admin/fajlfeltoltes.php
@@ -0,0 +1,17 @@
+<?php
+print_r($POST);
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Fájlfeltöltés</title>
+</head>
+<body>
+<h1>Fájlfeltöltés</h1>
+    <form method="post" action="">
+        <input type="file" id="fajl" name="fajl">
+    </form>
+</body>
+</html>
