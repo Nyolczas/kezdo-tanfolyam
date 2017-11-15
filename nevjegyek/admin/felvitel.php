1a889e95a6b75b685d61f8f2642bbb3ef82f0cb5
Nyolczas <nyolczas@gmail.com>
2017-11-15 22:29:27 +0100

    felvitel

--- nevjegyek/admin/felvitel.php
+++ nevjegyek/admin/felvitel.php
@@ -22,6 +22,15 @@ if (isset($_POST['rendben'])) {
 	if (!empty($email) && !filter_var($email, FILTER_VALIDATE_EMAIL)) 
 		$hibak[] = "Rossz e-mail címet adtál meg!";
 	
+	// Hibaüzenetek összeállítása
+	if (isset($hibak)) {
+		$kimenet = "<ul>\n";
+		foreach($hibak as $hiba) {
+			$kimenet.= "<li>{$hiba} </li>\n";
+		}
+		$kimenet.= "</ul>\n";
+	}
+	else { //ha nincs hiba akkor..
     // Adatbázis feltöltése
     require("../kapcsolat.php");
     $sql = "INSERT INTO nevjegyek
@@ -31,6 +40,7 @@ if (isset($_POST['rendben'])) {
     mysqli_query($dbconn, $sql);
     // átirányítás:
     header("Location: lista.php");
+	}
 }
 // Űrlap megjelenítése
 ?><!DOCTYPE html>
@@ -45,21 +55,22 @@ if (isset($_POST['rendben'])) {
 <body>
     <h1>Névjegykártyák</h1>
     <form method="post" action="">
+		<?php if (isset($kimenet)) print $kimenet; ?>
         <p> <label for="nev">Név*:</label>
             <br>
-            <input type="text" id="nev" name="nev" required>
+            <input type="text" id="nev" name="nev" value="<?php if (isset($nev)) print $nev; ?>" required>
         </p>
         <p> <label for="cegnev">Cégnév:</label>
             <br>
-            <input type="text" id="cegnev" name="cegnev">
+            <input type="text" id="cegnev" name="cegnev" value="<?php if (isset($nev)) print $nev; ?>">
         </p>
         <p> <label for="mobil">Mobil:</label>
             <br>
-            <input type="tel" id="mobil" name="mobil">
+            <input type="tel" id="mobil" name="mobil" value="<?php if (isset($nev)) print $nev; ?>">
         </p>
         <p> <label for="email">E-mail:</label>
             <br>
-            <input type="email" id="email" name="email">
+            <input type="email" id="email" name="email" value="<?php if (isset($nev)) print $nev; ?>">
         </p>
         <p><em>A *-gal jelölt mezők kitölése kötelező!</em></p>
         <input type="submit" id="rendben" name="rendben" value="Rendben">
